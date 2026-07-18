19ea4b895d9a4fd7c93880fc0e4a190998b4f64a
lichy2004 <lichengyang20040404@gmail.com>
2026-07-18 14:02:23 +0800

    [update] register

--- src/robohub/policies/__init__.py
+++ src/robohub/policies/__init__.py
@@ -1,8 +1,7 @@
-from robohub.policies.act import ACTPolicy
 from robohub.policies.base import Policy
 from robohub.policies.fake_policy import FakePolicy
-from robohub.policies.pi05 import PI05Policy
+from robohub.policies.act import ACTPolicy
 
-POLICY_REGISTRY = {"act": ACTPolicy, "fake_policy": FakePolicy, "pi05": PI05Policy}
+POLICY_REGISTRY = {"act": ACTPolicy, "fake_policy": FakePolicy}
 
-__all__ = ["ACTPolicy", "FakePolicy", "PI05Policy", "Policy", "POLICY_REGISTRY"]
+__all__ = ["ACTPolicy", "FakePolicy", "Policy", "POLICY_REGISTRY"]
--- src/robohub/robots/__init__.py
+++ src/robohub/robots/__init__.py
@@ -1,14 +1,10 @@
-from robohub.robots.astribot import Astribot
 from robohub.robots.base import Robot
-from robohub.robots.cobot_magic import CobotMagic
 from robohub.robots.fake_robot import FakeRobot
-from robohub.robots.unitree_g1 import UnitreeG1
+from robohub.robots.astribot import Astribot
 
 ROBOT_REGISTRY = {
     "astribot": Astribot,
-    "cobot_magic": CobotMagic,
     "fake_robot": FakeRobot,
-    "unitree_g1": UnitreeG1,
 }
 
-__all__ = ["Astribot", "CobotMagic", "FakeRobot", "Robot", "UnitreeG1", "ROBOT_REGISTRY"]
+__all__ = ["Astribot", "FakeRobot", "Robot", "ROBOT_REGISTRY"]
